1808d4ac236a1e9bbb28e0c1b8d98421c3e2115c
vladimir <serov.qa@gmail.com>
2023-08-09 17:27:30 +0300

    Allure report in pages 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,33 +14,33 @@ podcaster = PodcasterPage()
 
 
 
-@pytest.fixture(scope='function', autouse=True)
-def remote_browser():
-    browser.config.driver_name = 'chrome'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    yield
-    browser.quit()
-
-
 # @pytest.fixture(scope='function', autouse=True)
 # def remote_browser():
-#     capabilities = {
-#         "browserName": 'chrome',
-#         "browserVersion": '114.0',
-#         'enableVNC': True,
-#         'enableVideo': False,
-#         'screenResolution': '1920x1080x24',
-#         'sessionTimeout': '8m'
-#     }
-#     options = webdriver.ChromeOptions()
-#     options.set_capability('selenoid:options', capabilities)
-#     driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', options=options)
-#
-#     browser.config.driver = driver
-#     driver.maximize_window()
-#     yield driver
-#     driver.quit()
+#     browser.config.driver_name = 'chrome'
+#     browser.config.window_width = 1920
+#     browser.config.window_height = 1080
+#     yield
+#     browser.quit()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def remote_browser():
+    capabilities = {
+        "browserName": 'chrome',
+        "browserVersion": '114.0',
+        'enableVNC': True,
+        'enableVideo': False,
+        'screenResolution': '1920x1080x24',
+        'sessionTimeout': '8m'
+    }
+    options = webdriver.ChromeOptions()
+    options.set_capability('selenoid:options', capabilities)
+    driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', options=options)
+
+    browser.config.driver = driver
+    driver.maximize_window()
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture()
